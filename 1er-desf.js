9393c0ccc40c8c62c416b9ba3fb76c60ecf19f5c
Ge01ge <106602944+Ge01ge@users.noreply.github.com>
2022-08-28 20:22:56 -0300

    cambio de ejercicio

--- 1er-desf.js
+++ 1er-desf.js
@@ -1,50 +1,28 @@
 
-// LOGICA QUE FUNCIONA <Y CUMPLE> LA CONSIGNA
+let x = Number(prompt("  Ingrese valores del 1 al 6 para coorroborar la función: (y = x + x*2)"));
 
-let nro = Number(prompt("Ingrese un numero del 1 al 6"));
-parseInt(nro)
+while (x <= 6){
+    document.write( ` y= ${x + x*2}  <br>` );
+    x++;
+} 
+// si ingreso 7 no imprime nada ¿Como haria para que imprima un mj valor erroneo?
 
-for (let i=1; i<= nro; i++) {
-    if (i<=3) {
-        document.write(` ${i} pertenece al equipo ROJO <br>`);
-    }else if(i>3 && i<=6){
-        document.write(` ${i} pertenece al equipo VERDE <br>`);
-    }else{
-        document.write(` ${nro} valor invalido`)
-    }
-    /*¿Como hago para que aparezca un solo document.write?. 
-    Si uso los alert me ejecuta los 3 alert  y si uso un break me corta en el la 1ra ejecucion... si por ej elijo 6 me corta en rojo (cuando deberia saltar al 2do else it para que me de verde) */
+// -------------------------------------------
+
+/* 
+let x = Number(prompt("  Ingrese valores del 1 al 6 para coorroborar la función: (y = x + x*2)"));
+
+do{
+    document.write( ` y= ${x + x*2}  <br>` );
+    x++;
+} while (x <= 6){
 }
-
-
-// LOGICA QUE FUNCIONA PERO <NO CUMPLE> LA CONSIGNA  
-
-/*
-let option = prompt(`Elija un valor, "sabra su equipo al finalizar la elección":
-1: A
-2: B
-3: C
-4: D
-`);
-
-switch (option) {
-    case "1":
-        alert(`Ud esta en el Equipo ROJO`);
-        break;
-    case "2":
-        alert(`Ud esta en el Equipo VERDE`);
-        break;
-    case "3":
-        alert(`Ud esta en el equipo VERDE`);
-        break;
-    case "4":
-        alert(`Ud esta en el Equipo ROJO`);
-        break;
-    default:
-        alert(`Opcion no valida, vuelva a probar`);
-        break;
-}
+<<DUDA>>
+Si ingreso 7 no cumple la condicion pero ejecuta igual una vez por el "Do" dando y=21 
+¿Como haria para que imprima solo el 7 pero al no cumplir la condicion de "While" no imprima el y=21?
 */
+
+
 
 
 
