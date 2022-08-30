5dd7302ae7eace8e754a14be39ff4bf7fb4d224a
Ge01ge <106602944+Ge01ge@users.noreply.github.com>
2022-08-30 10:43:03 -0300

    corrección ejercicio

--- 1er-desf.js
+++ 1er-desf.js
@@ -1,26 +1,21 @@
 
-let x = Number(prompt("  Ingrese valores del 1 al 6 para coorroborar la función: (y = x + x*2)"));
+let x = Number(prompt("  Ingrese un valor inicial del (1 al 6) para coorroborar la función: y = x*3"));
+let y = Number(prompt("  Ingrese otro valor final del (1 al 6) para coorroborar la función: y = x*3"));
 
-while (x <= 6){
-    document.write( ` y= ${x + x*2}  <br>` );
-    x++;
-} 
 // si ingreso 7 no imprime nada ¿Como haria para que imprima un mj valor erroneo?
+// para validar valores mayores a 6 habría que usar un if:
 
-// -------------------------------------------
-
-/* 
-let x = Number(prompt("  Ingrese valores del 1 al 6 para coorroborar la función: (y = x + x*2)"));
-
-do{
-    document.write( ` y= ${x + x*2}  <br>` );
-    x++;
-} while (x <= 6){
+if(x > 6){
+    alert("Ingrese un valor inical menor o igual a 6")
+} else if(y > 6){
+    alert("Ingrese un valor final menor o igual a 6")
+}else {
+    while (x <= y){
+        document.write(  ` y= ${x*3} <br> ` );
+        x++;
+    }     
 }
-<<DUDA>>
-Si ingreso 7 no cumple la condicion pero ejecuta igual una vez por el "Do" dando y=21 
-¿Como haria para que imprima solo el 7 pero al no cumplir la condicion de "While" no imprima el y=21?
-*/
+
 
 
 
